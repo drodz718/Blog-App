f0bdea22b3b66533022eda6e6ac2b9a902a915b1
root <drodz718@gmail.com>
2020-08-01 18:07:27 +0000

    finish delete route and made some ui improvements

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser"),
 methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer"), 
 mongoose = require("mongoose"),
 express = require("express"),
 app = express();
@@ -16,6 +17,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 //MONGOOSE/ MODEL CONFIG
@@ -54,6 +56,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
 	//create blog
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog) {
 		if (err) {
 			res.render("new");
@@ -88,8 +91,9 @@ app.get("/blogs/:id/edit", function(req, res) {
 	});
 });
 
-//PUT ROUTE
+// PUT/UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
 		if(err) {
 			res.redirect("/blogs");
@@ -97,6 +101,18 @@ app.put("/blogs/:id", function(req, res) {
 			res.redirect("/blogs/" + req.params.id);
 		}
 	});
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res) {
+	//destry blogs 
+	Blog.findByIdAndRemove(req.params.id, function(err) {
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	})
 });
 
 // Blog.create({
